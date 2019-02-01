d6c1b8c4e72bea7a58683c69384cbcc887be3e0f
Marc McIntosh <email@marcmcintosh.ninja>
2019-02-01 21:46:37 +0100

    Ordering tests and canceld formula test

--- test/formual.js
+++ test/formual.js
@@ -3,7 +3,7 @@ const assert = require('assert');
 const matrix = require('../src/matrix');
 const formula = require('../src/formula');
 
-describe('formula', function() {
+xdescribe('formula', function() {
 	this.timeout(0);
 
 //	jsc.property('matrix(n) === recursive(n) Range 0 to 30', jsc.number(0, 30), n => matrix(n) === recursive(n));
